61ac630b16d9f266701abc1c7a2043d4a20a255c
Kevin Duell <kdubzot9@gmail.com>
2020-04-08 16:14:23 -0700

    Capture App with me image working on canvas.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import me from './images/meTransparent.png';
 import './App.css';
+import OrbitalCanvas from './OrbitalCanvas.js'
 
 
 import Background from './images/paper_half_lines.png';
 
 var sectionStyle = {
  width: "100%",
-  height: "400px",
+  height: "200px",
  backgroundImage: `url(${Background})`
 };
-
+ 
 function App() {
   return (
     <div class="App" style={ sectionStyle }>
       <h1>
           It's All About Me
         </h1>
-          <img src={me} className="App-logo" alt="my avatar" />
+          
+       
+         <OrbitalCanvas/>
+        
+          
+         
+    
      
     </div>
   );
