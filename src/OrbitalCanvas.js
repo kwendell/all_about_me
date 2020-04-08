849a43d889cdbae33f0d71b5c9bbd601dae2acb6
Kevin Duell <kdubzot9@gmail.com>
2020-04-08 14:37:52 -0700

    Capture OrbitalCanvas.js

---
+++ src/OrbitalCanvas.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class OrbitalCanvas extends Component {
+
+render() {
+  return(<canvas></canvas>);
+                                            
+  }
+  
+}
+
+export default OrbitalCanvas
