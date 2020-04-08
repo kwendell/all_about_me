c0c7dbb90e0e598ae1b244f8324fe258ce3db807
Kevin Duell <kdubzot9@gmail.com>
2020-04-08 15:04:50 -0700

    Capture OrbitalCanvas drawing.

--- src/OrbitalCanvas.js
+++ src/OrbitalCanvas.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import me from './images/meTransparent.png';
 
 class OrbitalCanvas extends Component {
+componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    const img = this.refs.image
+    
+     img.onload = () => {
+      ctx.drawImage(img, 200, 0)
+      ctx.font = "20px Courier"
+    
+    }
+  }
 
 render() {
-  return(<canvas></canvas>);
+  return(
+      <div>
+        <canvas ref="canvas" width="640" height="425" />
+        <img ref="image" src={me} className="hidden" />
+      </div>
+    )
                                             
   }
   
