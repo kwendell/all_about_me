1ee50e0c04a736defae7e43cab32f478740707d0
Kevin Duell <kdubzot9@gmail.com>
2020-04-12 09:16:42 -0700

    Capture App.js with lined paper styling.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import me from './images/meTransparent.png';
 import './App.css';
 import Animation from './Animation.js'
+import Background from './images/paper_half_lines.png';
 
-
+var sectionStyle = {
+ width: "100%",
+  height: "100%",
+ backgroundImage: `url(${Background})`
+};
 
  
  
 class App extends React.Component {
   render() {
-    return <div>
-      <Animation></Animation>
+    return <div style={sectionStyle}>
+      <Animation ></Animation>
     </div>;
   }
 }
