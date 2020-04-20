7b2904a101a3646a81e3257ccccd984a1023fe5a
Kevin Duell <kdubzot9@gmail.com>
2020-04-20 07:01:05 -0700

    Capture checkpoint of angle, r being corrrectly passed.

--- src/Animation.js
+++ src/Animation.js
@@ -19,20 +19,18 @@ class Animation extends React.Component {
   }
   
   updateAnimationState() {
-  //  this.setState(prevState => ({ angle: prevState.angle + 1 }));
+ 
     // find the next theta by solution for delta theta.
     const deltaTheta = 2*this.fractionalArea*Math.PI*((1-Math.cos(this.state.angle)/2)/150)**2;
-      this.rAF = requestAnimationFrame(this.updateAnimationState);
-    this.setState(prevState => ({angle:prevState.angle+deltaTheta }));
-  //  console.log(this.state.angle % 2*Math.PI);
+    this.rAF = requestAnimationFrame(this.updateAnimationState);
     
-   // const current_r = 150/(1-Math.cos(this.state.angle % 2*Math.PI)/2);
+     this.setState(prevState => ({ angle: (prevState.angle + deltaTheta) % 2*Math.PI }));
     this.setState(prevR => ({r:150/(1-Math.cos(this.state.angle % 2*Math.PI)/2)}));
+    
+  //  console.log(this.state.angle);
+    
   
-    //this.setState(({angle: newAngle });
-    this.setState(prevState => ({ angle: (prevState.angle + deltaTheta) % 2*Math.PI }));
- //   this.setState(prevR  
-//   console.log(this.state.r);
+  
     
   }
   
--- src/Canvas.js
+++ src/Canvas.js
@@ -7,25 +7,32 @@ import nato from './images/NATO.png';
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
+    console.log(props);
     this.canvasRef = React.createRef();
     this.imageRef = React.createRef();
+    this.angle = this.props.angle;
+    this.r=this.props.r;
+ //   console.log(this.r);
+  //  console.log(this.angle);
   }
   
   componentDidUpdate() {
-    const {angle} = this.props;
+    //const {angle} = this.props.angle;
     const canvas = this.canvasRef.current;
      const img = this.imageRef.current;
     const ctx = canvas.getContext('2d');
     const width = canvas.width;
     const height = canvas.height;
-  
+   //  console.log(this.r);
+   // console.log(this.angle);
     
     const r = 200;
     const translate_x=r;
     const translate_y=translate_x;
    // console.log(angle);
-    let x = r*Math.cos(angle);
-    let y = r*Math.sin(angle);
+    let x = r*Math.cos(this.props.angle);
+    let y = r*Math.sin(this.props.angle);
+    console.log(x);
     x+=translate_x;
     y+=translate_y;
    ctx.clearRect(x-4, y-4, img.width+8, img.height+8);  
