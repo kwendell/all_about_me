405943f4a03d0725d5e051028306dcf7970b9d1c
Kevin Duell <kdubzot9@gmail.com>
2020-05-12 20:27:50 -0700

    Finally showing both objects.

--- src/Animation.js
+++ src/Animation.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Canvas from './Canvas.js'
+
 import me from './images/meTransparent.png';
 import nato from './images/NATO.png';
 import pencil from './images/pencil.png';
@@ -12,12 +12,13 @@ class Animation extends React.Component {
     this.natoRef = React.createRef();
     this.meRef = React.createRef();
     this.pencilRef = React.createRef();
-    this.ellipseParms = [{semi_major_axis:100,eccentricity:.5,reference:null,deltaTheta:.01},{semi_major_axis:200,eccentricity:.7,reference:null,deltaTheta:.02}];
+    this.ellipseParms = [{semi_major_axis:100,eccentricity:.5,reference:null,deltaTheta:.01},
+      {semi_major_axis:200,eccentricity:.7,reference:null,deltaTheta:.03}];
     //this.ellipseParms[0].reference=this.natoRef;
 
 
     this.state = {
-      list: [{angle: 0, r:100},{angle: 0, r:100}],
+      list: [{angle: 0, r:100},{angle: 20, r:100}],
     };
 
     this.item_path=this.ellipseParms.item_path;
@@ -25,7 +26,6 @@ class Animation extends React.Component {
 
     this.updateAnimationState = this.updateAnimationState.bind(this);
 
-    this.eccentricity = .5;
 
 
 
@@ -37,7 +37,7 @@ class Animation extends React.Component {
   componentDidMount() {
     this.rAF = requestAnimationFrame(this.updateAnimationState);
     this.setState({r:this.ellipseParms[0].semi_major_axis});
-    this.setState({angle:0});
+//    this.setState({angle:0});
   }
 
   componentWillUnmount() {
@@ -55,45 +55,41 @@ class Animation extends React.Component {
     const currentTime = new Date();
     const deltaTime = currentTime-this.lastTime;
 
-    this.ellipseParms[1].reference = this.natoRef.current;
-    this.ellipseParms[0].reference = this.pencilRef.current;
-     const canvas = this.canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.ellipseParms[0].reference = this.natoRef.current;
+    this.ellipseParms[1].reference = this.pencilRef.current;
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+  const me = this.meRef.current;
+  var x = new Array(2);
+  var y = new Array(2);
     for (let i=0;i<this.ellipseParms.length;i++) {
-    if (deltaTime > 20 ) {
-      const deltaTheta=this.ellipseParms[i].deltaTheta;
-    //  console.log(deltaTheta);
-      elementClone[i].angle=deltaTheta+elementClone[i].angle ;
-       const p=this.ellipseParms[i].semi_major_axis*(1-this.ellipseParms[i].eccentricity**2);
+      if (deltaTime > 5 ) {
 
-      const newR = p/(1-this.ellipseParms[i].eccentricity*Math.cos(elementClone[i].angle));
+        const deltaTheta=this.ellipseParms[i].deltaTheta;
+        elementClone[i].angle=deltaTheta+elementClone[i].angle ;
+        const p=this.ellipseParms[i].semi_major_axis*(1-this.ellipseParms[i].eccentricity**2);
+        const newR = p/(1-this.ellipseParms[i].eccentricity*Math.cos(elementClone[i].angle));
 
+        elementClone[i].r=newR;
+        this.lastTime=currentTime;
 
-      elementClone[i].r=newR;
-      console.log(elementClone[i].r);
-      //elementClone.
-     this.setState({list:elementClone});
+        canvas.width = window.innerWidth;
 
-      this.lastTime=currentTime;
+        x[i] = this.state.r*Math.cos(this.state.list[i].angle);
+        y[i] = this.state.r*Math.sin(this.state.list[i].angle);
 
-
-      const me = this.meRef.current;
-
-      canvas.width = window.innerWidth;
-
-      const x = this.state.r*Math.cos(this.state.list[i].angle);
-      const y = this.state.r*Math.sin(this.state.list[i].angle);
-
-      const semi_minor_axis = this.ellipseParms[i].semi_major_axis*this.ellipseParms[i].eccentricity;
+        const semi_minor_axis = this.ellipseParms[i].semi_major_axis*this.ellipseParms[i].eccentricity;
+        const c = Math.sqrt(this.ellipseParms[i].semi_major_axis**2-semi_minor_axis**2);
+      //  console.log(x[i]);
 
 
-      const c = Math.sqrt(this.ellipseParms[i].semi_major_axis**2-semi_minor_axis**2);
-
-
-      ctx.drawImage(this.ellipseParms[0].reference,x+canvas.width/2-c , y+canvas.height/2-me.height/2);
-      ctx.drawImage(me,canvas.width/2-me.width/2,canvas.height/2-me.height/2);
       }
+      ctx.drawImage(this.ellipseParms[0].reference,x[0]+canvas.width/2 , y[0]+canvas.height/2-me.height/2);
+        ctx.drawImage(this.ellipseParms[1].reference,x[1]+canvas.width/2 , y[1]+canvas.height/2-me.height/2);
+       ctx.drawImage(me,canvas.width/2-me.width/2,canvas.height/2-me.height/2);
+
+
+//this.setState({list:elementClone})
 
 
   }
@@ -105,9 +101,9 @@ class Animation extends React.Component {
 
     render() {
     return <div><canvas width="1000" height="290"  ref={this.canvasRef} >
-    <img ref={this.natoRef} src={nato} className="hidden" />
-    <img ref={this.pencilRef} src={pencil} className="hidden" />
-    <img ref={this.meRef} src={me} className="hidden" />
+    <img ref={this.natoRef} src={nato} className="hidden" alt="NATO"/>
+    <img ref={this.pencilRef} src={pencil} className="hidden"  alt="pencil"/>
+    <img ref={this.meRef} src={me} className="hidden" alt="just me" />
     </canvas>
 
     </div>;
