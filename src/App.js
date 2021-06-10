35301a2ba1ff9a38c9c3d0e3ce0d690948277633
Kevin Duell <kdubzot9@gmail.com>
2021-06-10 08:14:39 -0700

    Add skills

--- src/App.js
+++ src/App.js
@@ -47,19 +47,55 @@ class App extends React.Component {
         </div>
 		<div >
 		<span class="work-experience-header">Store Intelligence (May 2005 – Present)</span>
-		<div >
-		<ul >
-		<li  >Developed authoring tool for the design of digital signage media.
-</li>
-<li>Authored user interface specification documents for handheld and desktop devices.</li>
-<li>Conducted customer demos; generated pre-sales artifacts.
-</li>
-<li>Led retailer integration efforts.</li>
-<li>Authored content generation service for digital signage service; oversaw cloud deployment of content generation service.</li>
-<li>Maintained digital signage content for several retailers.</li>
-<li>Release planning; customer liaison suport.</li>
+		
+		<ul>
+		<li>Developed authoring tool for the design of digital signage media.</li>
+        <li>Authored user interface specification documents for handheld and desktop devices.</li>
+        <li>Conducted customer demos; generated pre-sales artifacts.
+        </li>
+        <li>Led retailer integration efforts.</li>
+        <li>Authored content generation digital signage service; oversaw cloud deployment of content generation service.</li>
+        <li>Created and maintained digital signage content for retailers.</li>
+        <li>Planned, coordinated product releases.</li>
 		</ul>
-		</div>
+		
+		<span class="work-experience-header">Lightbridge/Altawave, Inc.(July 2000 – March 2005)</span>
+		
+		<ul>
+		<li>Developed application/user interface for AOL Instant Messenger 24 by 7 voice conferencing service (AIM® Voice Conferencing).</li>
+        <li>Responded to AIM® Voice Conferencing customer enhancements and modifications requests.</li>
+        <li>Followed fixes, enhancements through deployment providing necessary deployment configuration data, and/or documentation.
+        </li>
+        <li>Storyboarded, designed and developed a Java/Swing Graphical User Interface for application to monitor application and network level alarms within a Network Operations Center.</li>
+        <li>Led a team to consensus from use case analysis and u/i storyboarding efforts through the implementation of a web-based conferencing application.</li>
+      
+        <li>Led project development efforts.</li>
+		</ul>
+		
+		<span class="work-experience-header">Savi Technology - Sunnyvale, CA (Oct 98 – June 2000)</span>
+		
+		<ul>
+		<li>Developed Graphical User Interfaces using Java AWT/Swing for client/server shipping/transportation application.</li>
+        <li>Determined requirements, designed in the implementation of a client application.</li>
+        <li>Led system test efforts for system deployment.
+        </li>
+        <li>Supported on-site system installation, demonstration and test.</li>
+        <li>Led a team to consensus from use case analysis and u/i storyboarding efforts through the implementation of a web-based conferencing application.</li>
+        <li>Performed Business Process Analysis for a web-based client/server system.</li>
+    
+		</ul>
+		
+		<span class="work-experience-header">Storm Control Systems (Oct 95 – Oct 98)</span>
+		
+		<ul>
+		<li>Designed, implemented GUI for a distributed client-server product.</li>
+        <li>Developed and deployed a real-time application used by the DoD to compute satellite tracking offsets and update trajectories.</li>
+        <li>Designed and coded an application to compute spacecraft attitude from telemetered observation data.</li>
+        <li>Wrote and implemented system level test plans/procedures for a client-server control system.</li>
+     
+    
+		</ul>
+		
 		
 		</div>
 
